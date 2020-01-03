531d59d49c61e12d792f7171f51d96e1a07b4640
Prajwal_W <prajwalwaingankar@yahoo.in>
2020-01-03 19:08:42 +0530

    aabout us view model

--- app/src/main/java/com/prajwal/restaurant/ui/aboutUs/AboutUs_ViewModel.java
+++ app/src/main/java/com/prajwal/restaurant/ui/aboutUs/AboutUs_ViewModel.java
@@ -1,14 +1,14 @@
-package com.prajwal.restaurant.ui.slideshow;
+package com.prajwal.restaurant.ui.aboutUs;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class SlideshowViewModel extends ViewModel {
+public class AboutUs_ViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public SlideshowViewModel() {
+    public AboutUs_ViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is slideshow fragment");
     }
