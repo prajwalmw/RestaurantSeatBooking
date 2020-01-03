26dbaeb373db0804a3496483b792f250d6cd35b7
Prajwal_W <prajwalwaingankar@yahoo.in>
2020-01-03 19:59:40 +0530

    Rating Review

--- app/src/main/java/com/prajwal/restaurant/ui/review/ReviewFragment.java
+++ app/src/main/java/com/prajwal/restaurant/ui/review/ReviewFragment.java
@@ -4,19 +4,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.prajwal.restaurant.R;
 
+import static com.firebase.ui.auth.AuthUI.getApplicationContext;
+
 public class ReviewFragment extends Fragment {
 
     private ToolsViewModel toolsViewModel;
+    RatingBar ratingBar;
+    Button button;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -30,6 +35,29 @@ public class ReviewFragment extends Fragment {
                 textView.setText(s);
             }
         });*/
+       ratingBar = root.findViewById(R.id.ratingBar);
+       button = root.findViewById(R.id.button);
+       onStart();
         return root;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        addListenerOnButtonClick();
+    }
+
+    public void addListenerOnButtonClick(){
+        //Performing action on Button Click
+        button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View arg0) {
+                //Getting the rating and displaying it on the toast
+                String rating=String.valueOf(ratingBar.getRating());
+                Toast.makeText(getContext(), rating, Toast.LENGTH_LONG).show();
+            }
+
+        });
     }
 }
