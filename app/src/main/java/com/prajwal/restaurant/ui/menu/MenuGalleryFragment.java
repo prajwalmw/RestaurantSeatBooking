2426abc3e2f5f5dbb2d2e5316232c5dce21f58bb
prajwalmw <prajwalwaingankar@yahoo.in>
2020-02-08 18:07:25 +0530

    Menu/Gallery added.

---
+++ app/src/main/java/com/prajwal/restaurant/ui/menu/MenuGalleryFragment.java
@@ -0,0 +1,33 @@
+package com.prajwal.restaurant.ui.menu;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.prajwal.restaurant.R;
+import com.prajwal.restaurant.ui.aboutUs.AboutUs_ViewModel;
+
+public class MenuGalleryFragment extends Fragment {
+
+    private MenuGalleryViewModel menuGalleryViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        menuGalleryViewModel =
+                ViewModelProviders.of(this).get(MenuGalleryViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_menu_item, container, false);
+        // final TextView textView = root.findViewById(R.id.text_slideshow);
+        /*aboutUsViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });*/
+        return root;
+    }
+}
